bb3474a6e9abbb5175efd50ecbf9faa36928c2e4
AC3019 <55951264+AC3019@users.noreply.github.com>
2024-06-19 17:58:57 +0800

    done home page

--- FFM/obj/Debug/net8.0/FFM.AssemblyInfo.cs
+++ FFM/obj/Debug/net8.0/FFM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FFM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+689615907ad7e38e3048d7e7d7a7865cf46d1e1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9f9c9d0129585d427c2fdbbe060bb6208e3c41")]
 [assembly: System.Reflection.AssemblyProductAttribute("FFM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FFM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ FFM/wwwroot/js/index.js
@@ -0,0 +1,21 @@
+﻿let slideshowIndex = 0;
+startSlideShow();
+function startSlideShow() {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideshowIndex++;
+    if (slideshowIndex > slides.length) {
+        slideshowIndex = 1;
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideshowIndex - 1].style.display = "block";
+    dots[slideshowIndex - 1].className += " active";
+    setTimeout(startSlideShow, 4000);
+}
